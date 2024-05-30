c1e18b9736cadab27ae41ab00e5e4f3a5c3c0e5d
Parnassus84 <grizzly.huacchav@gmail.com>
2024-05-30 14:42:25 -0500

    feat(list): created components with test data

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from '../Header/Header';
 import { AddTask } from '../AddTask/AddTask';
-
 import { FilterTask } from '../Filter/FilterTask';
+import { TodoList } from '../TodoList/TodoList';
+import { Task } from '../../models/task';
+
+const testList = [
+  {
+    name: 'Task 1',
+    checked: false,
+  },
+  {
+    name: 'Task 2',
+    checked: true,
+  },
+  {
+    name: 'Task 3',
+    checked: false,
+  },
+  {
+    name: 'Task 4',
+    checked: true,
+  }
+];
+
 
 export const Todo = () => {
+  const [list, setList] = useState<Task[]>(testList);
 
 
   const onAddTask = (val: string) => {
@@ -15,13 +37,26 @@ export const Todo = () => {
     
   };
 
+  const onRemoveTask = (index: number) => {
+    setList([]);
+  };
+
+  const onCheckedTask = (index: number) => {
+    
+  };
+
   return (
     <div className="w-[500px] p-10 self-center">
       <Header className="mb-3 text-xl text-center">
         <h1>TO-DO List</h1>
       </Header>
       <AddTask className="mb-3 text-xl text-center" addTask={onAddTask} />
-      <FilterTask className="mb-3 text-xl" filterBy={onFilterBy} />      
+      <FilterTask className="mb-3 text-xl" filterBy={onFilterBy} />    
+      <TodoList
+        list={list}
+        onDeleted={onRemoveTask}
+        onChecked={onCheckedTask}
+      />  
     </div>
   );
 };
