9d8c68f5a35960cd13dee87ca40d0ac8bdc6dbee
Parnassus84 <grizzly.huacchav@gmail.com>
2024-06-02 18:51:50 -0500

    feat(services): added simulated cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense } from 'react';
 import { AppRoutes } from './pages/routes/app-routes';
 import { TodoProvider } from './contexts/provider';
+import { CircularProgress } from '@mui/material';
 
 function App() {
   return (
     <TodoProvider>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<CircularProgress />}>
         <AppRoutes />
       </Suspense>
     </TodoProvider>
--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,6 +1,7 @@
 import { useEffect, useMemo, useState } from 'react';
 import { ITask } from '../models';
 import { taskService } from '../services/task/task.service';
+import { useTodo } from '../contexts/context';
 
 export interface IUseTasks {
   todoId: string;
@@ -9,6 +10,9 @@ export interface IUseTasks {
 export const useTasks = ({ todoId }: IUseTasks) => {
   const [tasks, setTasks] = useState<Array<ITask>>();
   const [loading, setLoading] = useState(false);
+  const {state} = useTodo();
+  const selectedTodo = state.todos.find((todo) => todo.id === todoId);
+
   useEffect(() => {
     const fetchTasks = async () => {
       setLoading(true);
@@ -23,9 +27,9 @@ export const useTasks = ({ todoId }: IUseTasks) => {
         setLoading(false);
       }
     };  
-    fetchTasks();
+    if(!selectedTodo?.tasks) fetchTasks();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [todoId]);
+  }, []);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const memoizedData = useMemo(() => tasks, [tasks]);
   return { tasks: memoizedData, loading };
